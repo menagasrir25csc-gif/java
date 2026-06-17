6489b1c01c90e00506ff22ef8966b4bb58928219
menaga <menagasrir25csc@vetias.ac.in>
2026-06-17 16:13:56 +0530

    day11

--- src/day11.java
+++ src/day11.java
@@ -1,4 +1,4 @@
-import java.util.HashSet;
+/*import java.util.HashSet;
 import java.util.Set;
 
 public class day11 {
@@ -13,5 +13,23 @@ public class day11 {
         for(String s:set){
             System.out.println(s);
         }
+    }
+}*/
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class day11 {
+    public static void main(String[] args){
+        Map<String, Integer>Mark = new HashMap<>();
+        Mark.put("ram",89);
+        Mark.put("john",78);
+        Mark.put("ravi",98);
+        Mark.put("ramesh",100);
+        System.out.println(Mark.get("ravi"));
+        Mark.entrySet().forEach(entry ->{
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
     }
 }
