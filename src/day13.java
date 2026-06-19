7bc93184f1938085eda78a9560094401b129c600
menaga <menagasrir25csc@vetias.ac.in>
2026-06-19 15:57:12 +0530

    day13

--- src/day13.java
+++ src/day13.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+/*import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,4 +23,30 @@ import java.util.List;
             io.printStackTrace();
         }
     }
+} */
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class day13 {
+    static void main(){
+        Path path = Path.of("file","students.csv");
+        try {
+            List<String> students = Files.readAllLines(path);
+
+            for (String line : students) {
+                String[] row = line.split(",");
+                for(int i = 0; i < row.length; i++){
+                    System.out.print(row[i]+" | ");
+                }
+                System.out.println();
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+
 }
