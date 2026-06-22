fc263af261f3bbbd92ca10e9a960b78364102f54
menaga <menagasrir25csc@vetias.ac.in>
2026-06-22 14:39:32 +0530

    day14_factory

--- src/day14.java
+++ src/day14.java
@@ -53,3 +53,5 @@ public class day14 {
     }
 
 }
+
+
---
+++ src/day14_factory.java
@@ -0,0 +1,31 @@
+public class day14_factory {
+    interface Notification {
+        void send(String to, String message);
+    }
+
+    class EmailNotification implements Notification {
+        public void send(String to, String message) {
+            System.out.println("Sending Email: " + message + ", to " + to);
+        }
+    }
+
+    class WhatsAppNotification implements Notification {
+
+        public void send(String to, String message) {
+            System.out.println("Sending WhatsApp:" + message + ", to ");
+        }
+    }
+
+    static Notification getNotification(day14_factory factory, String type) {
+        return switch (type) {
+            case "Email" -> factory.new EmailNotification();
+            case "WhatsApp"-> factory.new WhatsAppNotification();
+            default -> throw new IllegalArgumentException("Invalid Notification type: " + type);
+        };
+    }
+    static void main() {
+        day14_factory factory = new day14_factory();
+        String type = "WhatsApp";
+        getNotification(factory,type).send("9787554458", "hello");
+    }
+}
